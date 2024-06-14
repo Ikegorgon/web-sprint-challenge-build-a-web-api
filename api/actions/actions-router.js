fe53ad570456f818ddc755e4df202696d98acf8c
Isaac Lamb <31518729+Ikegorgon@users.noreply.github.com>
2024-06-14 12:29:09 -0600

    Added Actions and Projects Routers

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,9 @@
 // Write your "actions" router here!
+const express = require('express');
+const router = express.Router();
+const actions = require('./actions-model');
+const middleware = require('./actions-middleware');
+
+
+
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,9 @@
 // Write your "projects" router here!
+const express = require('express');
+const router = express.Router();
+const projects = require('./projects-model');
+const middleware = require('./projects-middleware');
+
+
+
+module.exports = router;
